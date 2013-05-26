884ff7da921aee89b009cabd83312e52e16acfa4
Eric Kessler <morrow748@gmail.com>
2013-05-26 17:21:12 -0400

    FeatureElement updates
    Added specs around the FeatureElement class for better test
    coverage.

--- spec/unit/feature_element_specs.rb
+++ spec/unit/feature_element_specs.rb
@@ -6,14 +6,34 @@ shared_examples_for 'a feature element' do |clazz|
     @element = clazz.new
   end
 
-  it 'has a name - #name, #name=' do
+  it 'has a name - #name' do
     @element.should respond_to(:name)
-    @element.should respond_to(:name=)
   end
 
-  it 'has a description - #description, #description=' do
+  it 'can get and set its name - #name, #name=' do
+    @element.name = :some_name
+    @element.name.should == :some_name
+    @element.name = :some_other_name
+    @element.name.should == :some_other_name
+  end
+
+  it 'has a description - #description' do
     @element.should respond_to(:description)
-    @element.should respond_to(:description=)
+  end
+
+  it 'can get and set its description - #description, #description=' do
+    @element.description = :some_description
+    @element.description.should == :some_description
+    @element.description = :some_other_description
+    @element.description.should == :some_other_description
+  end
+
+  it 'starts with no name' do
+    @element.name.should == ''
+  end
+
+  it 'starts with no description' do
+    @element.description.should == []
   end
 
 end
