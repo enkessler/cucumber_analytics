30f17e6618fcd08e186cdf8772fc394e5c77504f
Eric Kessler <morrow748@gmail.com>
2013-05-26 22:03:54 -0400

    TestElement updates
    Added more specs around the TestElement class for better test
    coverage.

--- spec/unit/test_element_specs.rb
+++ spec/unit/test_element_specs.rb
@@ -7,14 +7,25 @@ shared_examples_for 'a test element' do |clazz|
   end
 
   it 'has steps - #steps' do
-     @element.should respond_to(:steps)
-   end
+    @element.should respond_to(:steps)
+  end
+
+  it 'can get and set its steps - #steps, #steps=' do
+    @element.steps = :some_steps
+    @element.steps.should == :some_steps
+    @element.steps = :some_other_steps
+    @element.steps.should == :some_other_steps
+  end
+
+  it 'starts with no steps' do
+    @element.steps.should == []
+  end
 
   it 'contains steps - #contains' do
     steps = [:step_1, :step_2, :step_3]
     @element.steps = steps
 
-    steps.each {|step| @element.contains.should include(step)}
+    steps.each { |step| @element.contains.should include(step) }
   end
 
 end
