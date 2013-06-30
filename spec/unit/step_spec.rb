b20344b4d6977a862c7c2df489543ab7af73c43c
Eric Kessler <morrow748@gmail.com>
2013-06-30 01:31:47 -0400

    Spec update
    Moved some of the Step unit specs to the Step integration specs now
    that Tables and DocStrings are their own classes.

--- spec/unit/step_spec.rb
+++ spec/unit/step_spec.rb
@@ -183,24 +183,6 @@ describe 'Step, Unit' do
     expect { clazz.new(source) }.to_not raise_error
   end
 
-  it 'can determine its equality with another Step' do
-    source_1 = "Given a test step with *parameter 1* and *parameter 2*\n|a block|"
-    source_2 = "Given a test step with *parameter 3* and *parameter 4*\n|another block|"
-    source_3 = 'Given a different *parameterized* step'
-
-    step_1 = CucumberAnalytics::Step.new(source_1)
-    step_2 = CucumberAnalytics::Step.new(source_2)
-    step_3 = CucumberAnalytics::Step.new(source_3)
-
-    step_1.delimiter = '*'
-    step_2.delimiter = '*'
-    step_3.delimiter = '*'
-
-
-    (step_1 == step_2).should be_true
-    (step_1 == step_3).should be_false
-  end
-
   context '#step_text ' do
 
     before(:each) do
@@ -219,30 +201,6 @@ describe 'Step, Unit' do
 
     it 'returns the step\'s text as an Array' do
       @step.step_text.is_a?(Array).should be_true
-    end
-
-    it 'returns the step\'s entire text by default' do
-      source = "Given a test step with -parameter 1- ^and@ *parameter 2!!\n|a block|"
-      step_with_block = CucumberAnalytics::Step.new(source)
-
-      expected_output = ['Given a test step with -parameter 1- ^and@ *parameter 2!!',
-                         '|a block|']
-
-      step_with_block.step_text.should == expected_output
-
-      source = 'Given a test step with -parameter 1- ^and@ *parameter 2!!'
-      step_without_block = CucumberAnalytics::Step.new(source)
-
-      expected_output = ['Given a test step with -parameter 1- ^and@ *parameter 2!!']
-
-      step_without_block.step_text.should == expected_output
-    end
-
-    it 'can provide the step\'s text without the keyword' do
-      expected_output = ['a test step with -parameter 1- ^and@ *parameter 2!!',
-                         '|a block|']
-
-      @step.step_text(with_keywords: false).should == expected_output
     end
 
     it 'can provide the step\'s text without the arguments' do
