d5dbef0c03d3ac1c238215f613c34473dcce4473
Eric Kessler <morrow748@gmail.com>
2013-10-02 22:29:40 -0400

    Code cleanup
    Minor whitespace tweaks.

--- spec/integration/step_integration_spec.rb
+++ spec/integration/step_integration_spec.rb
@@ -62,54 +62,54 @@ describe 'Step, Integration' do
   end
 
   it 'can determine its equality with another Step' do
-      source_1 = "Given a test step with *parameter 1* and *parameter 2*\n|a block|"
-      source_2 = "Given a test step with *parameter 3* and *parameter 4*\n|another block|"
-      source_3 = 'Given a different *parameterized* step'
+    source_1 = "Given a test step with *parameter 1* and *parameter 2*\n|a block|"
+    source_2 = "Given a test step with *parameter 3* and *parameter 4*\n|another block|"
+    source_3 = 'Given a different *parameterized* step'
 
-      step_1 = CucumberAnalytics::Step.new(source_1)
-      step_2 = CucumberAnalytics::Step.new(source_2)
-      step_3 = CucumberAnalytics::Step.new(source_3)
+    step_1 = CucumberAnalytics::Step.new(source_1)
+    step_2 = CucumberAnalytics::Step.new(source_2)
+    step_3 = CucumberAnalytics::Step.new(source_3)
 
-      step_1.delimiter = '*'
-      step_2.delimiter = '*'
-      step_3.delimiter = '*'
+    step_1.delimiter = '*'
+    step_2.delimiter = '*'
+    step_3.delimiter = '*'
 
 
-      (step_1 == step_2).should be_true
-      (step_1 == step_3).should be_false
+    (step_1 == step_2).should be_true
+    (step_1 == step_3).should be_false
   end
 
   context '#step_text ' do
 
-      before(:each) do
-        source = "Given a test step with -parameter 1- ^and@ *parameter 2!!\n|a block|"
-        @step = CucumberAnalytics::Step.new(source)
-      end
+    before(:each) do
+      source = "Given a test step with -parameter 1- ^and@ *parameter 2!!\n|a block|"
+      @step = CucumberAnalytics::Step.new(source)
+    end
 
 
-      it 'returns the step\'s entire text by default' do
-        source = "Given a test step with -parameter 1- ^and@ *parameter 2!!\n|a block|"
-        step_with_block = CucumberAnalytics::Step.new(source)
+    it 'returns the step\'s entire text by default' do
+      source = "Given a test step with -parameter 1- ^and@ *parameter 2!!\n|a block|"
+      step_with_block = CucumberAnalytics::Step.new(source)
 
-        expected_output = ['Given a test step with -parameter 1- ^and@ *parameter 2!!',
-                           '|a block|']
+      expected_output = ['Given a test step with -parameter 1- ^and@ *parameter 2!!',
+                         '|a block|']
 
-        step_with_block.step_text.should == expected_output
+      step_with_block.step_text.should == expected_output
 
-        source = 'Given a test step with -parameter 1- ^and@ *parameter 2!!'
-        step_without_block = CucumberAnalytics::Step.new(source)
+      source = 'Given a test step with -parameter 1- ^and@ *parameter 2!!'
+      step_without_block = CucumberAnalytics::Step.new(source)
 
-        expected_output = ['Given a test step with -parameter 1- ^and@ *parameter 2!!']
+      expected_output = ['Given a test step with -parameter 1- ^and@ *parameter 2!!']
 
-        step_without_block.step_text.should == expected_output
-      end
+      step_without_block.step_text.should == expected_output
+    end
 
-      it 'can provide the step\'s text without the keyword' do
-        expected_output = ['a test step with -parameter 1- ^and@ *parameter 2!!',
-                           '|a block|']
+    it 'can provide the step\'s text without the keyword' do
+      expected_output = ['a test step with -parameter 1- ^and@ *parameter 2!!',
+                         '|a block|']
 
-        @step.step_text(with_keywords: false).should == expected_output
-      end
+      @step.step_text(with_keywords: false).should == expected_output
+    end
 
   end
 
--- spec/unit/nested_element_unit_specs.rb
+++ spec/unit/nested_element_unit_specs.rb
@@ -10,12 +10,12 @@ shared_examples_for 'a nested element' do |clazz|
     @nested_element.should respond_to(:parent_element)
   end
 
-   it 'can get and set its parent element - #parent_element, #parent_element=' do
-     @nested_element.parent_element = :some_parent_element
-     @nested_element.parent_element.should == :some_parent_element
-     @nested_element.parent_element = :some_other_parent_element
-     @nested_element.parent_element.should == :some_other_parent_element
-   end
+  it 'can get and set its parent element - #parent_element, #parent_element=' do
+    @nested_element.parent_element = :some_parent_element
+    @nested_element.parent_element.should == :some_parent_element
+    @nested_element.parent_element = :some_other_parent_element
+    @nested_element.parent_element.should == :some_other_parent_element
+  end
 
   it 'starts with no parent element' do
     @nested_element.parent_element.should == nil
