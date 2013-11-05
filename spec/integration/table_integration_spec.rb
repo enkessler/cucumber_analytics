7188809f071248fab38b2587b40ea0d0459f6cbb
Eric Kessler <morrow748@gmail.com>
2013-11-05 17:46:19 -0500

    Spec update
    Added some additional specs for Table objects that should have been
    there from the start.

--- spec/integration/table_integration_spec.rb
+++ spec/integration/table_integration_spec.rb
@@ -17,4 +17,59 @@ describe 'Table, Integration' do
     row_2.parent_element.should equal table
   end
 
+  context 'getting stuff' do
+
+    before(:each) do
+      source = ['Feature: Test feature',
+                '',
+                '  Scenario: Test test',
+                '    * a step:',
+                '      | a | table |']
+      source = source.join("\n")
+
+      file_path = "#{@default_file_directory}/table_row_test_file.feature"
+      File.open(file_path, 'w') { |file| file.write(source) }
+
+      @directory = CucumberAnalytics::Directory.new(@default_file_directory)
+      @table = @directory.feature_files.first.features.first.tests.first.steps.first.block
+    end
+
+
+    it 'can get its directory' do
+      directory = @table.get_ancestor(:directory)
+
+      directory.path.should == @directory.path
+    end
+
+    it 'can get its feature file' do
+      feature_file = @table.get_ancestor(:feature_file)
+
+      feature_file.path.should == @directory.feature_files.first.path
+    end
+
+    it 'can get its feature' do
+      feature = @table.get_ancestor(:feature)
+
+      feature.name.should == @directory.feature_files.first.features.first.name
+    end
+
+    it 'can get its test' do
+      test = @table.get_ancestor(:test)
+
+      test.name.should == @directory.feature_files.first.features.first.tests.first.name
+    end
+
+    it 'can get its step' do
+      step = @table.get_ancestor(:step)
+
+      step.base.should == @directory.feature_files.first.features.first.tests.first.steps.first.base
+    end
+
+    it 'returns nil if it does not have the requested type of ancestor' do
+      example = @table.get_ancestor(:example)
+
+      example.should be_nil
+    end
+
+  end
 end
